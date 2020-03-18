28ad16afec395459edcf89eb1dfb951cb66f7006
Zaqueu C <zaqueudovale@gmail.com>
2020-03-17 23:02:41 -0300

    Add função de plotagem.

---
+++ plot_airfoil.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Source developed in Python 3.7
+Zaqueu Cavalcante (zaqueudovale@gmail.com) 
+Date: 17/03/2020
+"""
+
+import matplotlib.pyplot as plt
+
+def plot_airfoil(airfoil_points):
+    # Plota o perfil passado.
+    
+    
